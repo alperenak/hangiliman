2c02d004bcee22394564f4d502e59d29acdb87a9
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-07-22 18:44:14 +0300

    anchor animation added

--- src/components/part4/part4.js
+++ src/components/part4/part4.js
@@ -1,14 +1,12 @@
 import React from "react"
-
+import Phone from "../../assets/svg/phone.svg"
 export default function Part4() {
     return (
-        <div className="part4">
+        <div className="part4 flex-center flex-dir-col">
             <div className="contact-title"> <h1>Yüzdün yüzdün kuyruğuna geldin.</h1> </div>
             <div className="contact-paragraph"><p>Sana ulaşabilmemiz için alta bir numara bırak.</p></div>
-            <form>
-                <input/>
-                <div>Gönder</div>
-            </form>
+            <div className="contact-phone-input"><img src={Phone} /><input /></div>
+            <div>Gönder</div>
         </div>
     )
 }
