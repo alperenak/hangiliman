d403f1efdf3dac51e1dc44152358292dbc9225c6
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-07-22 06:15:28 +0300

    added animation in popular ports cards

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Home from "./home/home"
 import Part2 from "./part2/part2"
 import Part3 from "./part3/part3"
+import Part4 from "./part4/part4"
 
 export default function Index() {
     return (
@@ -9,6 +10,7 @@ export default function Index() {
             <Home />
             <Part2 />
             <Part3 />
+            <Part4 />
             <footer>
 
             </footer>
---
+++ src/components/part4/part4.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Part4() {
+    return (
+        <div className="part4">
+            <div className="contact-title"> <h1>Yüzdün yüzdün kuyruğuna geldin.</h1> </div>
+            <div className="contact-paragraph"><p>Sana ulaşabilmemiz için alta bir numara bırak.</p></div>
+            <form>
+                <input/>
+                <div>Gönder</div>
+            </form>
+        </div>
+    )
+}
