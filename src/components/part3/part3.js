473e58d1e6ff8401cdd2cb5fe2f518327732877f
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-07-22 05:41:14 +0300

    most popular ports added

--- src/components/part3/part3.js
+++ src/components/part3/part3.js
@@ -1,23 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import Bacground from "../../assets/images/part3.png"
 import Captain from "../../assets/images/captain.png"
+import PortCard from "./portCard"
 
 
 export default function Part3() {
-
+    const portData = useState([])
 
     return (
         <div className="part3">
 
             {/* background */}
-            <div className="captain-ship"><img src={Captain}/></div>
+            <div className="captain-ship"><img src={Captain} /></div>
             <div className="background-div">
-                <img src={Bacground}/>
+                <img src={Bacground} />
             </div>
 
             {/* Populer Limanlar */}
             <div className="part3-main flex-center">
-            <div className="popular-ports"><h1>Popüler Limanlar</h1></div>
+                <div className="popular-ports flex-dir-col"><h1>Popüler Limanlar</h1>
+                    {/* Liman cards */}
+                    <PortCard  />
+                </div>
             </div>
         </div>
     )
---
+++ src/components/part3/portCard.js
@@ -0,0 +1,117 @@
+import React from "react"
+import FirstPort from "../../assets/images/first.png"
+import SecondPort from "../../assets/images/second.png"
+import ThirdPort from "../../assets/images/third.png"
+import LocationIcon from "../../assets/svg/cardPortLocation.svg"
+export const fakeData = [
+    { order: 1, location: "Çeşme, İzmir", port: "Kaya Limanı", description: "2020 Yılında en çok tekne kiralanan 1. liman oldu." },
+    { order: 2, location: "Manavgat, Antalya", port: "Irmak Limanı", description: "2020 Yılında en çok tekne kiralanan 2. liman oldu." },
+    { order: 3, location: "San Francisco, ABD", port: "SK Limanı", description: "2020 Yılında en çok tekne kiralanan 3. liman oldu." }
+]
+export default function PortCard({ data = fakeData }) {
+
+    return (
+        <RenderCards data={data} />
+    )
+
+
+
+}
+function RenderCards({ data }) {
+    const Data = data.sort((a, b) => { return a.order - b.order })
+    console.log(Data)
+    return (
+        <div className="port-cards-container">
+
+            {/* second card */}
+
+            <div className="other-ports flex-dir-col">
+
+                {/* port filter */}
+
+                <div className="port-image-filter" />
+
+                {/* port image */}
+
+                <div className="port-image"><img src={SecondPort} /></div>
+
+                {/* port order */}
+
+                <div className="port-order ml-1 mb-1">#{Data[1].order}</div>
+
+                {/* port location */}
+
+                <div className="post-location flex-center ml-1"><img src={LocationIcon} />{Data[1].location}</div>
+
+                {/* port name */}
+
+                <div className="port-name ml-1">{Data[1].port}</div>
+
+                {/* port description */}
+
+                <div className="port-description ml-1 mb-1">{Data[1].description}</div>
+
+            </div>
+
+            {/* first card */}
+
+            <div className="first-port flex-dir-col">
+
+                {/* port filter */}
+
+                <div className="port-image-filter" />
+
+                {/* port image */}
+
+                <div className="port-image"><img src={FirstPort} /></div>
+
+                {/* port order */}
+
+                <div className="port-order ml-1 mb-1">#{Data[0].order}</div>
+
+                {/* port location */}
+                <div className="post-location flex-center ml-1"><img src={LocationIcon} />{Data[0].location}</div>
+
+                {/* port name */}
+
+                <div className="port-name ml-1">{Data[0].port}</div>
+
+                {/* port description */}
+
+                <div className="port-description ml-1 mb-1">{Data[0].description}</div>
+
+            </div>
+
+            {/* Third card */}
+
+            <div className="other-ports flex-dir-col">
+
+                {/* port filter */}
+
+                <div className="port-image-filter" />
+
+                {/* port image */}
+
+                <div className="port-image"><img src={ThirdPort} /></div>
+
+                {/* port order */}
+
+                <div className="port-order ml-1 mb-1">#{Data[2].order}</div>
+
+                {/* port location */}
+
+                <div className="post-location flex-center ml-1 "><img src={LocationIcon} />{Data[2].location}</div>
+
+                {/* port name */}
+
+                <div className="port-name ml-1 ">{Data[2].port}</div>
+
+                {/* port description */}
+
+                <div className="port-description ml-1 mb-1">{Data[2].description}</div>
+
+            </div>
+
+        </div>
+    )
+}
